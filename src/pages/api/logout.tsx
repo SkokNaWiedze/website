4c3638ab8fcee9bad96383a5ca4ef9e250bd0b16
„PersonalTaxi” <personaltaxiads@gmail.com>
2024-02-07 01:57:47 +0100

    first commit

--- src/pages/api/logout.tsx
+++ src/pages/api/logout.tsx
@@ -4,18 +4,19 @@ import { connectMongoDB } from "../../../libs/mongodb";
 import Session from "../../../models/sessions";
 
 export async function Logout(req: NextApiRequest, res: NextApiResponse) {
-  //   await cookie.serialize("_bagagwa", String("null"), {
-  //     // httpOnly: true,
-  //     maxAge: 1,
-  //     path: "/",
-  //   }),
-  // );
-
   await connectMongoDB();
   const SessionResult = await Session.findOneAndDelete({ login: req.body.login });
-  await res.setHeader("Set-Cookie", await cookie.serialize("_bagagwa", String("null")));
-
-  res.status(200).json({ msg: "Works" });
+  await res
+    .setHeader(
+      "Set-Cookie",
+      cookie.serialize("_bagagwa", String(`null`), {
+        // httpOnly: true,
+        maxAge: 1, // 1 week, 604800000 ms,
+        path: "/",
+      }),
+    )
+    .status(200)
+    .json({ msg: "Works" });
 }
 
 export default Logout;
